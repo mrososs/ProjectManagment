64272f8b42f4b331aa47d35b493b0b649f151a70
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-03-19 05:09:56 +0200

    feat : Create Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatCardModule, MatButtonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'projectManagment';
+}
