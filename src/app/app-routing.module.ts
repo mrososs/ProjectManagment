df8d8c0f8b4652a8cc4b5cc6fd3dc9aad14a2902
emanbadr <amonaelshafey252@gmail.com>
2025-03-20 16:44:51 +0200

    auto-redirect-to-auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+export const routes: Routes = [
 
-{ path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }
+  {path:'' , redirectTo:'auth' , pathMatch:'full'} ,
+  
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }
 
 ];
 @NgModule({
