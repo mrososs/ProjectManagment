d7de3dfc75a6f12eedf2b1fd7ee06612b805be6a
emanbadr <amonaelshafey252@gmail.com>
2025-03-20 17:35:51 +0200

    create-shared-module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MaterialModule } from './material/material.module';
+
+// import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule ,
+    FormsModule,
+    ReactiveFormsModule ,
+
+    MaterialModule
+  ] ,
+  exports:[
+    MaterialModule
+  ]
+})
+export class SharedModule { }
