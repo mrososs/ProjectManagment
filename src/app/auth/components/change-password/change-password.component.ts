ea9c0081dee4d7dd3e13c2b89059b7161d323d90
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-03-21 03:25:19 +0200

    adjustment the structure of assets folder

--- src/app/auth/components/change-password/change-password.component.ts
+++ src/app/auth/components/change-password/change-password.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-change-password',
   standalone: false,
-
   templateUrl: './change-password.component.html',
   styleUrl: './change-password.component.scss'
 })
