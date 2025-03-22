53e5216518c3ce9d1c6e2bcdbe8812ee2b5975a2
Muhammad Bakhit <mohamedbakhetoiu@gmail.com>
2025-03-22 04:24:37 +0200

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,10 +7,13 @@ import { ToastrModule } from 'ngx-toastr';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { httpRequestInterceptor } from './core/interceptors/http-request.interceptor';
 import { SharedModule } from './shared/shared.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -22,9 +25,12 @@ import { SharedModule } from './shared/shared.module';
       progressAnimation: 'increasing',
       easeTime: 300
     }),
-    SharedModule
+    SharedModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
   ],
-  providers: [provideHttpClient(withInterceptors([httpRequestInterceptor]))],
+  providers: [provideHttpClient(withInterceptors([httpRequestInterceptor])), provideAnimationsAsync()],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
