33f511d8074ef1ba8e7577a5c72ed6c5a9e51b8c
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-03-22 06:58:15 +0200

    chore : delete auth component

--- src/app/auth/components/auth/auth.component.ts
+++ src/app/auth/components/auth/auth.component.ts
@@ -6,14 +6,4 @@ import { FormControl, FormGroup } from '@angular/forms';
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
-export class AuthComponent {
-  loginForm!: FormGroup;
-  constructor() {
-    this.loginForm = new FormGroup({
-      email: new FormControl(''), // Changed from emailControl to email
-    });
-  }
-  get emailControl(): FormControl {
-    return this.loginForm.get('email') as FormControl;
-  }
-}
+export class AuthComponent {}
