bdec9097696185350326710b47e30dc9e216bd91
emanbadr <amonaelshafey252@gmail.com>
2025-03-24 08:15:18 +0200

    check custom validator

--- src/app/shared/components/custom-form-field/custom-form-field.component.ts
+++ src/app/shared/components/custom-form-field/custom-form-field.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
-import { AbstractControl, FormControl } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { getErrorMessage } from '../../services/validation-service.service';
+import { hasError } from '../../services/validation-service.service';
+
+
 
 @Component({
   selector: 'app-custom-form-field',
@@ -13,4 +18,19 @@ export class CustomFormFieldComponent {
   @Input() control!: FormControl; // Keep this as FormControl
   @Input() type: string = 'text'; // New Input Property with Default "text"
 
+  @Input() matcher: ErrorStateMatcher = new MyErrorStateMatcher; // Validators
+
+ @Input() hasError :boolean |any  = new hasError;
+ @Input()  getErrorMessage   :string | null |any = new getErrorMessage;
+
+
 }
+// -----------------------------------------------------
+
+
+  // custom Error
+  export class MyErrorStateMatcher implements ErrorStateMatcher {
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+      return !!(control && control.invalid && (control.touched  || control.dirty));
+    }
+  }
