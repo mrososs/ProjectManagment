591d532f29493b9046175d32cb1981df1dfd8139
emanbadr <amonaelshafey252@gmail.com>
2025-03-25 07:41:22 +0200

    create interfaces

---
+++ src/app/dashboard/interfaces/users-task.ts
@@ -0,0 +1,12 @@
+export interface ITaskCount {
+  toDo: number,
+  inProgress: number,
+  done: number
+}
+
+export interface IUsersCount{
+  activatedEmployeeCount: number,
+  deactivatedEmployeeCount: number
+}
+
+
