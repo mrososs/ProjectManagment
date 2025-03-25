88c229a8f896ce1b4d6477c70f7b2631e181760d
emanbadr <amonaelshafey252@gmail.com>
2025-03-25 05:00:04 +0200

    handel change password

--- src/app/auth/components/change-password/change-password.component.ts
+++ src/app/auth/components/change-password/change-password.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { CustomFormFieldComponent } from '../../../shared/components/custom-form-field/custom-form-field.component';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-change-password',
@@ -16,23 +18,34 @@ export class ChangePasswordComponent {
     confirmNewPassword: new FormControl('', {}),
   });
 
-  constructor(private _AuthService: AuthService) {}
+  constructor(private _AuthService: AuthService ,
+    private _ToastrService:ToastrService
+  ) {}
 
   getControl(controlName: string): FormControl {
     return this.changePasswordForm.get(controlName) as FormControl;
   }
 
-  onchangePassword(form: any) {
-    console.log(form);
-    //  this._AuthService.onChangingPassword().subscribe({
-    //   next:(res)=>{
-    //     console.log(res);
-    //   },
-    //   error:(err)=>{
-    //     console.log(err);
-    //   },
-    //   complete :()=>{
-    //   }
-    // })
+  onchangePassword(form: FormGroup) {
+    if (this.changePasswordForm.valid) {
+      console.log(form);
+     this._AuthService.onChangingPassword(form.value).subscribe({
+      next:(res)=>{
+        console.log(res);
+        this._ToastrService.success('Password has been Updated')
+      },
+      error:(err)=>{
+        console.log(err);
+        this._ToastrService.error(`Can't update Password`)
+      },
+      complete :()=>{
+      }
+    })
+    }
+    else{
+      console.log('Form is invalid!');
+     }
+
+
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -11,11 +11,12 @@ Observable;
 export class AuthService {
   constructor(private _HttpClient: HttpClient) {}
 
-  // onChangingPassword():Observable<any>{
-  //   return this._HttpClient.put(`https://upskilling-egypt.com:3003/api/v1/Users/ChangePassword`   )
-  // }
+
   isLoggedIn(): boolean {
     return !!localStorage.getItem('token'); // Example: Check if token exists
+  }
+   login(user: ILogin): Observable<ILogin> {
+    return this._HttpClient.post<ILogin>(`Users/Login`, user);
   }
   register(data: any): Observable<any> {
     return this._HttpClient.post('Users/Register', data);
@@ -23,14 +24,13 @@ export class AuthService {
   forgetPassword(email: string): Observable<string> {
     return this._HttpClient.post<string>('Users/Reset/Request', { email });
   }
-
   verify(data: any): Observable<any> {
     return this._HttpClient.put('Users/verify', data);
   }
   onResetPassword(UserParams: any): Observable<any> {
     return this._HttpClient.post(`Users/Reset/Request`, UserParams );
   }
-  login(user: ILogin): Observable<ILogin> {
-    return this._HttpClient.post<ILogin>(`Users/Login`, user);
+ onChangingPassword(UserData: any):Observable<any>{
+    return this._HttpClient.put(`Users/ChangePassword` , UserData)
   }
 }
