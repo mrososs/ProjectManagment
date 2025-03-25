f0d4b914903606a2a69b4225edccd4291421d53c
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-03-25 15:08:53 +0200

    edit routing and dashbaord

--- src/app/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorgeService } from '../../../core/services/storge.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  standalone:false ,
-  styleUrl: './dashboard.component.scss'
+  standalone: false,
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  constructor(private _storgeService: StorgeService, private _router: Router) {}
 
+  ngOnInit(): void {
+    if (this._storgeService.isEmployee()) {
+      this._router.navigate(['/dashboard/manager']);
+    } else if (this._storgeService.isManager()) {
+      this._router.navigate(['/dashboard/manager']);
+    }
+  }
 }
