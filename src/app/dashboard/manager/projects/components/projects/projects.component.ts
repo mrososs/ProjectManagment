848fe4620d7e1b377d7d09c9c9c0e5860724ee82
emanbadr <amonaelshafey252@gmail.com>
2025-03-25 09:21:36 +0200

    create projects & tasks Modules ( services)

---
+++ src/app/dashboard/manager/projects/components/projects/projects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  standalone: false ,
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+
+}
---
+++ src/app/dashboard/manager/tasks/components/tasks/tasks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  standalone :false ,
+  styleUrl: './tasks.component.scss'
+})
+export class TasksComponent {
+
+}
