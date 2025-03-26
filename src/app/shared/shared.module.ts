6ac67485f508cae264cf03525a47d7a9b1a8c707
Muhammad Bakhit <mohamedbakhetoiu@gmail.com>
2025-03-26 02:28:44 +0200

    update

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,27 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material/material.module';
-<<<<<<< HEAD
 import { provideHttpClient,withInterceptorsFromDi,} from '@angular/common/http';
-=======
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
-// import { ToastrModule } from 'ngx-toastr';
-
->>>>>>> origin/main
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CustomFormFieldComponent } from './components/custom-form-field/custom-form-field.component';
 import { RouterModule } from '@angular/router';
 import { FilePreviewComponent } from './components/file-preview/file-preview.component';
 import { ValidateDirective } from './directives/validate.directive';
 import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
-<<<<<<< HEAD
-import { MatCardModule } from '@angular/material/card';
-=======
-import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
->>>>>>> origin/main
 
 @NgModule({
   declarations: [
@@ -33,17 +19,14 @@ import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
   imports: [
     CommonModule,
     FormsModule,
-    CanvasJSAngularChartsModule, // Import CanvasJS Module
 
     ReactiveFormsModule,
     MaterialModule,
     RouterModule,
-    MatCardModule,
   ],
   providers: [provideHttpClient(withInterceptorsFromDi())],
   exports: [
     MaterialModule,
-    CanvasJSAngularChartsModule, // Import CanvasJS Module
     SplashScreenComponent,
     ValidateDirective,
     FilePreviewComponent,
