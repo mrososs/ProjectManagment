a7be0f339b7b0d98196a69f26da72aa1c6b5eb4e
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-03-26 16:06:51 +0200

    feat :create projects modules

--- src/app/dashboard/manager/projects/projects.component.ts
+++ src/app/dashboard/manager/projects/projects.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class ProjectsComponent {
-
+  searchForm!: FormGroup;
+  constructor(){
+    this.searchForm = new FormGroup({
+      search: new FormControl(''),
+    });
+  }
 }
