0a05eb1373d5e1316601ae20a9c52789a6fb93b9
Muhammad Bakhit <mohamedbakhetoiu@gmail.com>
2025-03-26 10:25:42 +0200

    uplode

--- src/app/dashboard/manager/manager.module.ts
+++ src/app/dashboard/manager/manager.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ManagerRoutingModule } from './manager-routing.module';
 import { ManagerComponent } from './manager.component';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { ManagerComponent } from './manager.component';
   ],
   imports: [
     CommonModule,
-    ManagerRoutingModule
+    ManagerRoutingModule,
+    SharedModule
   ]
 })
 export class ManagerModule { }
---
+++ src/app/dashboard/manager/tasks/tasks.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { TasksRoutingModule } from './tasks-routing.module';
+import { TasksComponent } from './tasks.component';
+import { AddEditTaskComponent } from './components/add-edit-task/add-edit-task.component';
+import { SharedModule } from '../../../shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    TasksComponent,
+    AddEditTaskComponent
+  ],
+  imports: [
+    CommonModule,
+    TasksRoutingModule,
+    SharedModule,
+    MatSelectModule
+  ]
+})
+export class TasksModule { }
