e7ab8c7ba1b6c465a1997e9f5ea6a656cd79f14a
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-04-05 14:29:53 +0200

    feat : create routing

--- src/app/dashboard/components/sidebar/sidebar.component.ts
+++ src/app/dashboard/components/sidebar/sidebar.component.ts
@@ -43,7 +43,7 @@ export class SidebarComponent implements OnInit {
     {
       name: 'Tasks',
       icon: 'event_note',
-      route: 'admin/categories',
+      route: 'manager/tasks',
       isAdmin: true,
     },
   
